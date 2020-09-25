756c6177756f4c43063eefa7bbb1386bfafd7376
Hemant Wagh <hemantwagh07@gmail.com>
2020-09-26 00:02:06 +0530

    Updates

---
+++ precache-manifest.b2261215646bc25ea0c6320fad3c5a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44779ff03bf4ea94c90da52b20ab4fdd",
+    "url": "/image-gallery/index.html"
+  },
+  {
+    "revision": "360d1bb717f1207e77c8",
+    "url": "/image-gallery/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "ee706cc7cea4e2c5aca4",
+    "url": "/image-gallery/static/css/main.04bb0d5b.chunk.css"
+  },
+  {
+    "revision": "360d1bb717f1207e77c8",
+    "url": "/image-gallery/static/js/2.da67ff5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image-gallery/static/js/2.da67ff5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee706cc7cea4e2c5aca4",
+    "url": "/image-gallery/static/js/main.8d2d70ec.chunk.js"
+  },
+  {
+    "revision": "4d9d01404d8fd2ce1307",
+    "url": "/image-gallery/static/js/runtime-main.c99468fd.js"
+  }
+]);
